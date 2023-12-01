c76dab858583fa8c38ebe4311763a2ee7e469fa1
michelin19m <mm19022000@gmail.com>
2023-12-01 19:43:18 +0200

    Refactored geolocation stimulus controller

--- app/javascript/controllers/geolocation_controller.js
+++ app/javascript/controllers/geolocation_controller.js
@@ -6,20 +6,30 @@ export default class extends Controller {
 
   static targets = ['property']
   connect() {
-    let lat = ""
-    let lon = ""
     if(isEmpty(this.element.dataset.latitude) && isEmpty(this.element.dataset.longitude)) {
-      window.navigator.geolocation.getCurrentPosition((position) => {
-        lat = position.coords.latitude
-        lon = position.coords.longitude
-      })
-    } else {
-      lat = this.element.dataset.latitude
-      lon = this.element.dataset.longitude
+      this.setUserCoordinates();
     }
+    this.setDisplayText()
+  }
+
+  setUserCoordinates() {
+    window.navigator.geolocation.getCurrentPosition((position) => {
+      this.element.dataset.latitude = position.coords.latitude
+      this.element.dataset.longitude = position.coords.longitude
+    })
+  }
+
+  getUserCoordinates() {
+    return {
+      latitude: this.element.dataset.latitude,
+      longitude: this.element.dataset.longitude
+    }
+  }
+
+  setDisplayText() {
     this.propertyTargets.forEach((propertyTarget) => {
       let distanceFrom = getDistance(
-        { latitude: lat, longitude: lon },
+        this.getUserCoordinates(),
         { latitude: propertyTarget.dataset.latitude, longitude: propertyTarget.dataset.longitude }
       );
       propertyTarget.querySelector('[data-distance-away]').innerHTML = `${convertDistance(distanceFrom, 'km').toFixed()} kilometers away`;
